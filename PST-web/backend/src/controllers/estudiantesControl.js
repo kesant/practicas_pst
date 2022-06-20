5bddcf22f3b88a61f9463631968d255bb2eedaa7
kevin santana <kevin_santana120@hotmail.com>
2022-06-19 21:37:05 -0500

    i created the funtions to make the requests http

--- PST-web/backend/src/controllers/estudiantesControl.js
+++ PST-web/backend/src/controllers/estudiantesControl.js
@@ -2,5 +2,44 @@ const db = require("./database.js");
 const estControl = {};
 
 estControl.getEstudiantes = (req,res)=>res.json(db.estudiantes);
+estControl.getEstudiante = (req,res)=>{
+    const estudiante = db.estudiantes.find(
+        (est)=>est.id == req.params.id
+    );
+
+    res.json(estudiante);
+}
+
+estControl.postEstudiante = (req,res)=>{
+    const {id, nombre,apellido} = req.body;
+    if(!id || !nombre || !apellido){
+        res.status(400).send("Datos incompletos {id, nombre, apellido}");
+        return;
+    }
+    const estudiante = {
+        id,
+        nombre,
+        apellido
+    }
+    db.estudiantes.push(estudiante);
+    db.updateDB();
+    res.send('Estudiante ingresado con éxito');
+}
+
+
+estControl.putEstudiante = (req,res)=>res.json({mensaje: "Estudiante actualizado"});
+estControl.deleteEstudiante = (req,res)=>{
+    const index = db.estudiantes.findIndex(
+        (est)=>est.id == req.params.id
+    );
+    if(index < 0){
+        res.status(400).send("Id de estudiante no encontrado");
+        return;
+    }
+    db.estudiantes.splice(index,1);
+    db.updateDB();
+    res.send('Estudiante eliminado');
+}
+
 
 module.exports = estControl;
--- PST-web/backend/src/routes/estudiantes.js
+++ PST-web/backend/src/routes/estudiantes.js
@@ -3,6 +3,13 @@ const router = Router();
 
 const {getEstudiantes,getEstudiante,postEstudiante,putEstudiante,deleteEstudiante} = require('../controllers/estudiantesControl.js');
 
-router.route('/').get(getEstudiantes)
+router.route('/')
+    .get(getEstudiantes)
+    .post(postEstudiante);
+    
+router.route('/:id')
+    .get(getEstudiante)
+    .put(putEstudiante)
+    .delete(deleteEstudiante);
 
 module.exports = router
